49acd790f0997aaebe307657f096eb2056773241
litoninot12345@gmail.com <litoninot12345@gmail.com>
2022-03-11 15:04:01 +0700

    add logging

--- flask2/main.py
+++ flask2/main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
+import logging
 
 import os 
 
@@ -10,6 +11,12 @@ app = Flask(
     __name__,
     static_url_path='', 
     static_folder='static'
+)
+
+logging.basicConfig(
+    filename='logging.log', 
+    level=logging.WARNING, 
+    format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s'
 )
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://"+ os.getenv("DB_USERNAME") + ":" + os.getenv("DB_PASSWORD") + "@" + os.getenv("DB_HOST") + "/" + os.getenv("DB_DATABASE")
--- flask2/views/auth.py
+++ flask2/views/auth.py
@@ -1,35 +1,45 @@
 from app import app
 from validators.signin_validator import SigninValidation
+from validators.signup_validator import SignupValidation
 from models import User
-from flask import jsonify,make_response
+from flask import jsonify,make_response, request
+from main import db
+import bcrypt
 
 class Auth:
-	# @app.route("/signin",methods=["POST"])
-	# def user_auth_signin_actions():
-	# 	form = SigninValidation(request.form)
-
-	# 	if not form.validate():
-	# 		flash({
-	# 			"status" : "failed",
-	# 			"message" : form.errors[list(form.errors.keys())[0]][0]
-	# 		})
-	# 		return redirect("/")
-
-	# 	user = User.query.filter_by(email=request.form['email']).first()
-
-	# 	if user == None:
-	# 		return redirect("/")
-
-	# 	session["user"] = user.id
-	# 	return redirect("/user")
-
     @app.route("/signin",methods=["POST"])
     def auth_signin():
         return make_response(jsonify({"message" : "Signin"}), 200)
     
     @app.route("/signup",methods=["POST"])
     def auth_signup():
-        return make_response(jsonify({"message" : "Signup"}), 200)
+        try:
+            form = SignupValidation(request.form)
+
+            if not form.validate():
+                return make_response(jsonify({
+                    "message" : form.errors[list(form.errors.keys())[0]][0]
+                }),422)
+
+
+            salt = bcrypt.gensalt()
+
+            hashed = bcrypt.hashpw(b""+request.form['password']+"", salt)
+
+            db.session.add(User(request.form['name'],request.form['email'],hashed))
+
+            db.session.commit() 
+
+            return make_response(jsonify({"message" : True}), 200)
+        except Exception as e:
+            db.session.rollback()
+
+            print(e)
+
+            app.logger.warning(e)
+
+            return make_response(jsonify({"message" : "Terjadi Kesalahan"}),500)            
+
 
     @app.route("/forgot-password",methods=["POST"])
     def auth_forgot_password():
